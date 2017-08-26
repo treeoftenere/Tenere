bfa893ef15cbaecb933b8defbda7012361df4a70
Nate Argetsinger <narget@umich.edu>
2017-08-26 03:52:34 -0700

    pingscan fixed for macs

--- UTIL/pingscan.sh
+++ UTIL/pingscan.sh
@@ -1,1 +1,1 @@
-for i in {2..254} ;do (ping 192.168.1.$i -c 1 -w 5  >/dev/null && echo "\"192.168.1.$i\"," &);done
+for i in {2..254} ;do (ping 192.168.1.$i -c 1 >/dev/null && echo "\"192.168.1.$i\"," &);done
